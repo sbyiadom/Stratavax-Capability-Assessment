d3de6c23c98f7fae1226dc4132061a79cf0fcfa2
sbyiadom <sbyiadom88@gmail.com>
2026-02-08 08:40:18 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Add this if you're having middleware issues
+  experimental: {
+    // Remove if causing issues
+  }
 };
 
 module.exports = nextConfig;
