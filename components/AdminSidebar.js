3276ca5f75cb2b40e610fa2d72bb11bc54b2aed8
sbyiadom <sbyiadom88@gmail.com>
2026-09-16 21:35:42 +0000

    Add 'Bulk Assign' option to AdminSidebar

--- components/AdminSidebar.js
+++ components/AdminSidebar.js
@@ -49,6 +49,14 @@ const menuItems = [
     )
   },
   {
+    id: 'bulk-assign',
+    label: 'Bulk Assign',
+    href: '/admin/bulk-assign',
+    icon: (
+      <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M20 8v6"/><path d="M17 11h6"/></svg>
+    )
+  },
+  {
     id: 'question-bank',
     label: 'Question Bank',
     href: '/admin/question-bank',
