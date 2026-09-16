d4eafb6f68d9debdeae2eab5f51e82d1e654d8b8
sbyiadom <sbyiadom88@gmail.com>
2026-09-16 12:37:36 +0000

    Add Templates link to admin navigation

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -482,6 +482,14 @@ function Sidebar({ isOpen, toggleSidebar, activePage, setActivePage, handleLogou
       href: '/admin/assessments'
     },
     { 
+      id: 'templates', 
+      label: 'Templates', 
+      icon: (
+        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><rect x="3" y="3" width="18" height="18" rx="2"/><line x1="3" y1="9" x2="21" y2="9"/><line x1="9" y1="21" x2="9" y2="9"/></svg>
+      ),
+      href: '/admin/templates'
+    },
+    { 
       id: 'roles', 
       label: 'Roles', 
       icon: (
